fcfb817fcdd3d1ae8a90bf99df73c1e72e32a3be
刘燕保 <liuyanbao@shuidihuzhu.com>
2020-07-11 23:16:40 +0800

    feat: 模拟支付模块

--- src/views/cashier.vue
+++ src/views/cashier.vue
@@ -1,19 +1,30 @@
 <!--
  * @Author: liuyanbao
  * @Date: 2020-06-18 16:33:10
- * @LastEditTime: 2020-06-18 21:20:06
+ * @LastEditTime: 2020-07-11 22:17:15
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /ssr_pricinple/ssr-technology/src/views/cashier.vue
 -->
 <template>
-  <div>
+  <div @click="clickfn">
     this is cashier
   </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    clickfn() {
+      console.log(133243)
+    }
+  },
+  created() {
+    console.log('执行了created')
+  },
+  mounted() {
+    console.log('执行了mounted')
+  }
 }
 </script>
 
